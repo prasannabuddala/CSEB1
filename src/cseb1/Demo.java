89bff5f10535522d9f5be0d1e1376dad9af84a08
prasa <prasa@LAPTOP-H0PSVFQQ>
2022-10-15 20:00:05 +0530

    second commit

--- src/cseb1/Demo.java
+++ src/cseb1/Demo.java
@@ -1,10 +1,10 @@
 package cseb1;
 
-public class Demo1 {
+public class Demo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hello,world!");
+		System.out.println("welcome to git");
 
 	}
 
