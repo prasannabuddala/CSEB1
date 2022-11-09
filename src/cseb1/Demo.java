9863a94f73c8f870640835106fa0181184a93860
prasannabuddala <82028155+prasannabuddala@users.noreply.github.com>
2022-11-09 11:57:14 +0530

    Create Demo.java
    jenkins

--- src/cseb1/Demo.java
+++ src/cseb1/Demo.java
@@ -5,7 +5,7 @@ public class Demo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("welcome to git................");
-		System.out.println("hello");
+		System.out.println("hello jenkins");
 
 	}
 
